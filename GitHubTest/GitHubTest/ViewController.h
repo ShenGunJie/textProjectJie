345da08f664f3d858400e1ab4fb296fad7b5304b
ShenGunJie <1979431224@qq,com>
2017-07-29 19:55:17 +0800

    first push
    test push

---
+++ GitHubTest/GitHubTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitHubTest
+//
+//  Created by 神棍 on 2017/7/29.
+//  Copyright © 2017年 神棍. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ GitHubTest/GitHubTest/ViewController.m
@@ -0,0 +1,32 @@
+//
+//  ViewController.m
+//  GitHubTest
+//
+//  Created by 神棍 on 2017/7/29.
+//  Copyright © 2017年 神棍. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    NSLog(@"github test");
+    
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
